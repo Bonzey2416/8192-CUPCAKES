ed996fa28a93b3124210c684a37372656863083b
Glukascewic1 <53239491+Glukascewic1@users.noreply.github.com>
2020-04-22 10:34:37 -0700

    Update Kcal and 8192 fixes

--- js/game_manager.js
+++ js/game_manager.js
@@ -167,7 +167,7 @@ GameManager.prototype.addStartTiles = function () {
 // Adds a tile in a random position
 GameManager.prototype.addRandomTile = function () {
   if (this.grid.cellsAvailable()) {
-    var value = Math.random() < 0.9 ? 2 : 4;
+    var value = Math.random() < 0.9 ? 2 : 32;
     var tile = new Tile(this.grid.randomAvailableCell(), value);
 
     this.grid.insertTile(tile);
@@ -271,8 +271,8 @@ GameManager.prototype.move = function (direction) {
           self.points += kcal(tile.value) * 2;
           if (merged.value > self.score) self.score = merged.value;
 
-          // The mighty 2048 tile
-          if (merged.value === 2048 || merged.value === 2048) self.won = true;
+          // The mighty 8192 tile
+          if (merged.value === 8192 || merged.value === 8192) self.won = true;
         } else {
           self.moveTile(tile, positions.farthest);
         }
